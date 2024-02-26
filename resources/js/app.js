0aefce90f7d9a0f1d6d944c29a950c1847f1e1b5
Pol Crespo <pol.crespo.hdez@gmail.com>
2024-02-26 19:22:43 +0100

    project structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,11 @@
 import './bootstrap';
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import panelUser from './components/admin/panelUser.vue';
 
+const app = createApp({});
+app.use(PrimeVue);
+app
+    .component('panelUser', panelUser)
 
+app.mount('#app');
---
+++ resources/js/components/admin/panelUser.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="m-5">
+        <h1>COMPONENT</h1>
+    </div>
+</template>
+  
+  <script>
+  export default {
+  
+    data() {
+      return {
+       
+      };
+    },
+    methods: {
+      
+    },
+    mounted() {
+      
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
