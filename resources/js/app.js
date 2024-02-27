02742b17b93b28ae1fec880fd2be70aefdd16f6e
Pol Crespo <pol.crespo.hdez@gmail.com>
2024-02-27 16:39:21 +0100

    PROVISIONAL NAV

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import * as bootstrap from 'bootstrap';
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
+import navMenu from './components/shared/nav.vue';
 import panelUser from './components/admin/panelUser.vue';
 import panelMenu from './components/providers/panelMenu.vue';
 import panelDelivery from './components/riders/panelDelivery.vue';
@@ -13,6 +14,7 @@ app.use(PrimeVue, {
 });
 const appDOM = document.getElementById('app');
 
+app.component('navmenu', navMenu)
 app.component('paneluser', panelUser)
 app.component('panelmenu', panelMenu)
 app.component('paneldelivery', panelDelivery)
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ Route::get('admin', function () {
 Route::get('menu', function () {
     return view('providers.menu');
 });
-Route::get('delivery', function () {
-    return view('riders.delivery');
+Route::get('riderlanding', function () {
+    return view('landings.riderlanding');
+});
+Route::get('providerlanding', function () {
+    return view('landings.providerlanding');
+});
+Route::get('login', function () {
+    return view('auth.login');
+});
+Route::get('register', function () {
+    return view('auth.register');
 });
