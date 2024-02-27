793faa4d17e4433730bbf8f46fa0a70100f8b1f4
Mario Molina <molinaballesteros@hotmail.com>
2024-02-27 16:43:00 +0100

    primera prueba administración

--- resources/js/components/admin/panelUser.vue
+++ resources/js/components/admin/panelUser.vue
@@ -1,27 +1,188 @@
+
 <template>
-    <div>
-        <h1>COMPONENT</h1>
-    </div>
+  <Card>
+    <template #content>
+      <DataTable v-model:filters="filters" :value="customers" paginator :rows="10" dataKey="id" filterDisplay="row" :loading="loading"
+              :globalFilterFields="['name', 'country.name', 'representative.name', 'status']">
+          <template #header>
+              <div class="flex justify-content-end">
+                  <IconField iconPosition="left">
+                      <InputIcon>
+                          <i class="pi pi-search" />
+                      </InputIcon>
+                      <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+                  </IconField>
+              </div>
+          </template>
+          <template #empty> No customers found. </template>
+          <template #loading> Loading customers data. Please wait. </template>
+          <Column field="name" header="Name" style="min-width: 12rem">
+              <template #body="{ data }">
+                  {{ data.name }}
+              </template>
+              <template #filter="{ filterModel, filterCallback }">
+                  <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Search by name" />
+              </template>
+          </Column>
+          <Column header="Country" filterField="country.name" style="min-width: 12rem">
+              <template #body="{ data }">
+                  <div class="flex align-items-center gap-2">
+                      <img alt="flag" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`flag flag-${data.country.code}`" style="width: 24px" />
+                      <span>{{ data.country.name }}</span>
+                  </div>
+              </template>
+              <template #filter="{ filterModel, filterCallback }">
+                  <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Search by country" />
+              </template>
+          </Column>
+          <Column header="Agent" filterField="representative" :showFilterMenu="false" :filterMenuStyle="{ width: '14rem' }" style="min-width: 14rem">
+              <template #body="{ data }">
+                  <div class="flex align-items-center gap-2">
+                      <img :alt="data.representative.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`" style="width: 32px" />
+                      <span>{{ data.representative.name }}</span>
+                  </div>
+              </template>
+              <template #filter="{ filterModel, filterCallback }">
+                  <MultiSelect v-model="filterModel.value" @change="filterCallback()" :options="representatives" optionLabel="name" placeholder="Any" class="p-column-filter" style="min-width: 14rem" :maxSelectedLabels="1">
+                      <template #option="slotProps">
+                          <div class="flex align-items-center gap-2">
+                              <img :alt="slotProps.option.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`" style="width: 32px" />
+                              <span>{{ slotProps.option.name }}</span>
+                          </div>
+                      </template>
+                  </MultiSelect>
+              </template>
+          </Column>
+          <Column field="status" header="Status" :showFilterMenu="false" :filterMenuStyle="{ width: '14rem' }" style="min-width: 12rem">
+              <template #body="{ data }">
+                  <Tag :value="data.status" :severity="getSeverity(data.status)" />
+              </template>
+              <template #filter="{ filterModel, filterCallback }">
+                  <Dropdown v-model="filterModel.value" @change="filterCallback()" :options="statuses" placeholder="Select One" class="p-column-filter" style="min-width: 12rem" :showClear="true">
+                      <template #option="slotProps">
+                          <Tag :value="slotProps.option" :severity="getSeverity(slotProps.option)" />
+                      </template>
+                  </Dropdown>
+              </template>
+          </Column>
+          <Column field="verified" header="Verified" dataType="boolean" style="min-width: 6rem">
+              <template #body="{ data }">
+                  <i class="pi" :class="{ 'pi-check-circle text-green-500': data.verified, 'pi-times-circle text-red-400': !data.verified }"></i>
+              </template>
+              <template #filter="{ filterModel, filterCallback }">
+                  <TriStateCheckbox v-model="filterModel.value" @change="filterCallback()" />
+              </template>
+          </Column>
+      </DataTable>
+    </template>
+  </Card>
+      
 </template>
-  
-  <script>
-  export default {
-  
-    data() {
-      return {
-       
-      };
+
+<script>
+import { FilterMatchMode } from 'primevue/api';
+import { CustomerService } from './CustomerService';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import InputText from 'primevue/inputtext';
+import InputIcon from 'primevue/inputicon';
+import IconField from 'primevue/iconfield';
+import MultiSelect from 'primevue/multiselect';
+import Dropdown from 'primevue/dropdown';
+import Tag from 'primevue/tag';
+import TriStateCheckbox from 'primevue/tristatecheckbox';
+import Card from 'primevue/card';
+
+
+export default {
+  data() {
+    return {
+      customers: null,
+      filters: {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        'country.name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        representative: { value: null, matchMode: FilterMatchMode.IN },
+        status: { value: null, matchMode: FilterMatchMode.EQUALS },
+        verified: { value: null, matchMode: FilterMatchMode.EQUALS }
+      },
+      representatives: [
+        { name: 'Amy Elsner', image: 'amyelsner.png' },
+        { name: 'Anna Fali', image: 'annafali.png' },
+        { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+        { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+        { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+        { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+        { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+        { name: 'Onyama Limba', image: 'onyamalimba.png' },
+        { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+        { name: 'XuXue Feng', image: 'xuxuefeng.png' }
+      ],
+      statuses: ['unqualified', 'qualified', 'new', 'negotiation', 'renewal', 'proposal'],
+      loading: true
+    };
+  },
+  methods: {
+    getCustomers(data) {
+      return [...(data || [])].map((d) => {
+        d.date = new Date(d.date);
+        return d;
+      });
     },
-    methods: {
-      
+    formatDate(value) {
+      return value.toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      });
     },
-    mounted() {
-      
+
+    formatCurrency(value) {
+      return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
     },
-  };
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+
+    getSeverity(status) {
+      switch (status) {
+        case 'unqualified':
+          return 'danger';
+        case 'qualified':
+          return 'success';
+        case 'new':
+          return 'info';
+        case 'negotiation':
+          return 'warning';
+        case 'renewal':
+          return null;
+      }
+    }
+  },
+  mounted() {
+    CustomerService.getCustomersMedium().then((data) => {
+      this.customers = this.getCustomers(data);
+      this.loading = false;
+    });
+  },
+
+  components: {
+    DataTable,
+    Column,
+    InputText,
+    InputIcon,
+    IconField,
+    MultiSelect,
+    Dropdown,
+    Tag,
+    TriStateCheckbox,
+    Card
+  } 
+};
+</script>
+
+<style>
+.p-card-body{
+  box-shadow: 0 0 0 1px #e0e0e0;
+  margin: 35px;
+  border-radius: 15px;
+}
+</style>
