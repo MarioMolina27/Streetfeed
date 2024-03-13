2dcf79f2debf393605af368a25ecb92fbfdb048f
Pol Crespo <pol.crespo.hdez@gmail.com>
2024-03-13 20:22:41 +0100

    New components... (Starting delivery)

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import panelUser from './components/admin/panelUser.vue';
 import panelMenu from './components/providers/panelMenu.vue';
-import panelDelivery from './components/riders/explore/panelDelivery.vue';
+import panelExplore from './components/riders/explore/panelExplore.vue';
 import providerDetails from './components/riders/reserve/providerDetails.vue';
 import assignLocation from './components/riders/reserve/assignLocation.vue';
 import adminHome from './components/admin/adminHome.vue';
@@ -15,13 +15,15 @@ import ConfirmationService from 'primevue/confirmationservice';
 import reserveConfirmation from './components/riders/reserve/reserveConfirmation.vue';
 import panelFavorite from './components/riders/explore/panelFavorite.vue';
 import login from './components/auth/login.vue';
+import delivery from './components/riders/delivery/delivery.vue';
+import profile from './components/shared/profile.vue';
 
 const app = createApp({}).use(PrimeVue).use(ConfirmationService);
 const appDOM = document.getElementById('app');
 
 app.component('paneluser', panelUser)
 app.component('panelmenu', panelMenu)
-app.component('paneldelivery', panelDelivery)
+app.component('panelexplore', panelExplore)
 app.component('providerdetails', providerDetails)
 app.component('assignlocation', assignLocation)
 app.component('adminhome', adminHome)
@@ -30,5 +32,7 @@ app.component('riderstats', riderStats)
 app.component('reserveconfirmation', reserveConfirmation)
 app.component('panelfavorite', panelFavorite)
 app.component('login', login)
+app.component('delivery', delivery)
+app.component('profile', profile)
 
 app.mount(appDOM);
